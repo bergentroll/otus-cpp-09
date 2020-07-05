119c5465335a6cc3ca5e40a10c72af3866d7e362
Anton Karmanov <bergertroll@insiberia.net>
2020-07-05 18:40:17 +0500

    Add test cases for LazyDigest

---
+++ test/test.cpp
@@ -0,0 +1,47 @@
+#include <filesystem>
+#include <fstream>
+#include <gtest/gtest.h>
+
+#include "lazy_digest.hpp"
+
+using namespace std;
+using namespace otus;
+namespace fs = filesystem;
+
+TEST(lazy_digest, equal) {
+  fs::path path { "test_file_equal.txt" };
+  ofstream f { path };
+  for(int i { }; i < 10; ++i)
+    f << to_string(i) << " Dolorem ipsum dolor sit amet";
+  f.close();
+
+  LazyDigest dig1 { path, 21 };
+  LazyDigest dig2 { path, 21 };
+
+  EXPECT_TRUE(dig1 == dig2);
+}
+
+TEST(lazy_digest, not_equal) {
+  fs::path path1 { "test_file_not_equal_1.txt" };
+  fs::path path2 { "test_file_not_equal_2.txt" };
+  ofstream f1 { path1 };
+  ofstream f2 { path2 };
+  for(int i { }; i < 10; ++i) {
+    f1 << to_string(i) << " Dolorem ipsum dolor sit amet";
+    f2 << to_string(i) << " Dolorem ipsum dolor sit amet";
+  }
+  f1 << '!'; f2 << '?';
+  f1.close(); f2.close();
+
+  LazyDigest dig1 { path1, 21 };
+  LazyDigest dig2 { path2, 21 };
+
+  EXPECT_TRUE(dig1 != dig2);
+}
+
+TEST(lazy_digest, different_block_size) {
+  fs::path path { "test_file_block_size.txt" };
+  ofstream f { path };
+  LazyDigest dig1 { path, 20 }, dig2 { path, 21 };
+  EXPECT_THROW(dig1 == dig2, LazyDigest::BlockSizeError);
+}
