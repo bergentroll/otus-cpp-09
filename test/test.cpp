ba559f1a06ff3a276307528724cd6f2561d3c28a
Anton Karmanov <bergertroll@insiberia.net>
2020-07-10 12:07:52 +0500

    Switch GTest -> Boost.Test

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,8 @@
+#define BOOST_TEST_MAIN
+
+#include <boost/test/unit_test.hpp>
 #include <filesystem>
 #include <fstream>
-#include <gtest/gtest.h>
 
 #include "lazy_digest.hpp"
 
@@ -8,7 +10,9 @@ using namespace std;
 using namespace otus;
 namespace fs = filesystem;
 
-TEST(lazy_digest, equal) {
+BOOST_AUTO_TEST_SUITE(LazyDigestTest);
+
+BOOST_AUTO_TEST_CASE(equal) {
   fs::path path { "test_file_equal.txt" };
   ofstream f { path };
   for(int i { }; i < 10; ++i)
@@ -18,10 +22,10 @@ TEST(lazy_digest, equal) {
   LazyDigest dig1 { path, 21 };
   LazyDigest dig2 { path, 21 };
 
-  EXPECT_TRUE(dig1.matches(dig2));
+  BOOST_CHECK(dig1.matches(dig2));
 }
 
-TEST(lazy_digest, not_equal) {
+BOOST_AUTO_TEST_CASE(not_equal) {
   fs::path path1 { "test_file_not_equal_1.txt" };
   fs::path path2 { "test_file_not_equal_2.txt" };
   ofstream f1 { path1 };
@@ -36,21 +40,23 @@ TEST(lazy_digest, not_equal) {
   LazyDigest dig1 { path1, 21 };
   LazyDigest dig2 { path2, 21 };
 
-  EXPECT_FALSE(dig1.matches(dig2));
+  BOOST_CHECK(!dig1.matches(dig2));
 }
 
-TEST(lazy_digest, different_block_size) {
+BOOST_AUTO_TEST_CASE(different_block_size) {
   fs::path path { "test_file_block_size.txt" };
   ofstream f { path };
   LazyDigest dig1 { path, 20 }, dig2 { path, 21 };
-  EXPECT_THROW(dig1.matches(dig2), LazyDigest::BinaryOpIncompatibility);
+  BOOST_CHECK_THROW(dig1.matches(dig2), LazyDigest::BinaryOpIncompatibility);
 }
 
-TEST(lazy_digest, different_digest_func) {
+BOOST_AUTO_TEST_CASE(different_digest_func) {
   fs::path path { "test_file_digest_func.txt" };
   ofstream f { path };
   LazyDigest
     dig1 { path, 20, make_crc_digest<boost::crc_16_type> },
     dig2 { path, 20, make_crc_digest<boost::crc_32_type> };
-  EXPECT_THROW(dig1.matches(dig2), LazyDigest::BinaryOpIncompatibility);
+  BOOST_CHECK_THROW(dig1.matches(dig2), LazyDigest::BinaryOpIncompatibility);
 }
+
+BOOST_AUTO_TEST_SUITE_END();
