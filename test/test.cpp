df39204ceb3094aa8cef8df72727a8b76309dc61
Anton Karmanov <bergertroll@insiberia.net>
2020-07-19 16:31:18 +0500

    Revert "Exterminate"
    This reverts commit 6af09a8bb37c286c90a36b9c94e92f8f08a88e8b.

---
+++ test/test.cpp
@@ -0,0 +1,68 @@
+/* To the extent possible under law, bayan has waived all copyright and related
+ * or neighboring rights to program.
+ *
+ * Written by 2020 Anton Karmanov <bergentroll@insiberia.net>.
+ */
+
+#define BOOST_TEST_MAIN
+
+#include <boost/test/unit_test.hpp>
+#include <filesystem>
+#include <fstream>
+
+#include "lazy_digest.hpp"
+
+using namespace std;
+using namespace otus;
+namespace fs = filesystem;
+
+BOOST_AUTO_TEST_SUITE(LazyDigestTest);
+
+BOOST_AUTO_TEST_CASE(equal) {
+  fs::path path { "test_file_equal.txt" };
+  ofstream f { path };
+  for(int i { }; i < 10; ++i)
+    f << to_string(i) << " Dolorem ipsum dolor sit amet";
+  f.close();
+
+  LazyDigest dig1 { path, 21 };
+  LazyDigest dig2 { path, 21 };
+
+  BOOST_CHECK(dig1.matches(dig2));
+}
+
+BOOST_AUTO_TEST_CASE(not_equal) {
+  fs::path path1 { "test_file_not_equal_1.txt" };
+  fs::path path2 { "test_file_not_equal_2.txt" };
+  ofstream f1 { path1 };
+  ofstream f2 { path2 };
+  for(int i { }; i < 10; ++i) {
+    f1 << to_string(i) << " Dolorem ipsum dolor sit amet";
+    f2 << to_string(i) << " Dolorem ipsum dolor sit amet";
+  }
+  f1 << '!'; f2 << '?';
+  f1.close(); f2.close();
+
+  LazyDigest dig1 { path1, 21 };
+  LazyDigest dig2 { path2, 21 };
+
+  BOOST_CHECK(!dig1.matches(dig2));
+}
+
+BOOST_AUTO_TEST_CASE(different_block_size) {
+  fs::path path { "test_file_block_size.txt" };
+  ofstream f { path };
+  LazyDigest dig1 { path, 20 }, dig2 { path, 21 };
+  BOOST_CHECK_THROW(dig1.matches(dig2), LazyDigest::BinaryOpIncompatibility);
+}
+
+BOOST_AUTO_TEST_CASE(different_digest_func) {
+  fs::path path { "test_file_digest_func.txt" };
+  ofstream f { path };
+  LazyDigest
+    dig1 { path, 20, make_crc_digest<boost::crc_16_type> },
+    dig2 { path, 20, make_crc_digest<boost::crc_32_type> };
+  BOOST_CHECK_THROW(dig1.matches(dig2), LazyDigest::BinaryOpIncompatibility);
+}
+
+BOOST_AUTO_TEST_SUITE_END();
